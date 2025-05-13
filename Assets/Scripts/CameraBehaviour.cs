d8d4dbf774fe587bf793054e796dd7af47b53d6f
GCeSilva <goncalo.c.silva@hotmail.com>
2025-05-13 21:50:32 +0100

    Orbital camera done

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -7,35 +7,29 @@ public class CameraBehaviour : MonoBehaviour
 {
 
     [SerializeField] Transform target; // The target to follow
-    [SerializeField] static float distance = 5.0f; // Distance from the target
+    [SerializeField] float distance = 10.0f; // Distance from the target
     [SerializeField] float sensitivity = 5.0f; // Mouse sensitivity
-    private Vector3 offset = new Vector3(distance, distance, distance); // Offset from the target
+    private Vector2 rotation; // Rotation of the camera
     
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = target.position + offset;
+        transform.position = target.position + new Vector3(distance, distance, distance); // Set initial position of the camera
         transform.LookAt(target); // Make the camera look at the target
+        rotation = new Vector2(transform.eulerAngles.x, transform.eulerAngles.y); // Initialize rotation
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x_input = Input.GetAxis("Mouse X");
-        float y_input = Input.GetAxis("Mouse Y");
+        rotation.y += Input.GetAxis("Mouse X") * sensitivity; // Update rotation based on mouse input
+        rotation.x -= Input.GetAxis("Mouse Y") * sensitivity; // Update rotation based on mouse input
 
-        transform.RotateAround(target.position, Vector3.up, x_input * sensitivity); // Rotate around the target
-        
-        if (transform.rotation.eulerAngles.x > -(Mathf.PI/2) && transform.rotation.x < Mathf.PI/2)
-            transform.RotateAround(target.position, transform.right, y_input * sensitivity); // Rotate around the target
-        else
-            transform.rotation = new Quaternion(
-                Mathf.Clamp(transform.rotation.x, -(Mathf.PI/2), Mathf.PI/2),
-                transform.rotation.y,
-                0,
-                transform.rotation.w
-            ); // Lock the camera rotation on the z-axis
+        rotation.x = Mathf.Clamp(rotation.x, -90, 90); // Clamp the x rotation to prevent flipping
 
+        transform.eulerAngles = new Vector3(rotation.x, rotation.y, 0); // Apply rotation to the camera
+
+        transform.position = target.position - transform.forward * distance;
     }
 }
