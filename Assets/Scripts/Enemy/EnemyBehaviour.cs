4c526326c93d63c22f7bac2a52258a702a8277d8
GCeSilva <goncalo.c.silva@hotmail.com>
2025-05-29 17:57:38 +0100

    NavMesh Agent added and Enemy tuned

--- Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -1,42 +1,34 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    [SerializeField] float speed = 200.0f;
     private Transform player;
-    private Vector3 movementdir;
-    private Rigidbody rb;
+    private NavMeshAgent agent;
     private ParticleSystem ps;
     private bool isDead = false;
+
+    private float ogSpeed;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        rb = GetComponent<Rigidbody>();
         ps = GetComponentInChildren<ParticleSystem>();
+        agent = GetComponent<NavMeshAgent>();
+        ogSpeed = agent.speed;
     }
 
-    void FixedUpdate()
+    void Update()
     {
         // stops behaviour if in assembly mode
         if (CameraManager.isAssemblyMode || isDead)
         {
-            rb.velocity = Vector3.zero;
+            agent.speed = 0;
             return;
-        }
+        } else agent.speed = ogSpeed;
 
-        Vector3 direction = (player.position - transform.position).normalized;
-        Vector3 playerpos = player.position;
-        playerpos.y = 0;
-        Vector3 enemypos = transform.position;
-        enemypos.y = 0;
 
-        if (Vector3.Distance(enemypos, playerpos) > 1.05f)
-        {
-            float fixedDeltaTime = Time.fixedDeltaTime;
-            movementdir = direction * speed * fixedDeltaTime;
-            rb.velocity = new Vector3(movementdir.x, rb.velocity.y, movementdir.z);
-        }
+        agent.SetDestination(player.position);
     }
 
     public void Death()
