379030cb05da6dcbda69d85aeaa428d64d258262
Bernardo Damasceno <bernardo.damasceno@tecnico.ulisboa.pt>
2025-06-12 16:30:52 +0100

    little refactor of the rockets code

--- Assets/Scripts/Gun/Missile.cs
+++ Assets/Scripts/Gun/Missile.cs
@@ -4,8 +4,8 @@ public class Missile : MonoBehaviour
 {
     private Transform parentTransform;
     private Rigidbody rb;
-    protected GameObject player;
 
+    [Header("Rocket Stats")]
     [SerializeField] private float missileSpeed = 0.2f;
     [SerializeField] private float hitDamage = 5.0f;
     [SerializeField] private float explosionDamage = 30.0f;
@@ -17,16 +17,16 @@ public class Missile : MonoBehaviour
     private Vector3 playerForward;
     private bool isExploded = false;
 
+    [Header("Particle FX's")]
     [SerializeField] private ParticleSystem explosionPs;
     [SerializeField] private ParticleSystem trusterPs;
 
     void Start()
     {
         parentTransform = GameObject.FindGameObjectWithTag("Cameraholder").transform;
-        player = GameObject.FindGameObjectWithTag("Player");
         playerForward = parentTransform.forward;
         transform.position += playerForward * extraDistance;
-        this.transform.Rotate(90.0f, 0.0f, 0.0f, Space.Self);
+        transform.Rotate(90.0f, 0.0f, 0.0f, Space.Self);
         rb = GetComponent<Rigidbody>();
     }
 
@@ -36,50 +36,54 @@ public class Missile : MonoBehaviour
 
         Vector3 position = transform.position + (playerForward * missileSpeed);
         rb.MovePosition(position);
-        
     }
 
     // this might need a refactor
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag != "PlayerGroup" && other.tag != "PlasmaOrb" && other.tag != "Missile")
-        {
-            print(other.tag);
-            if (other.CompareTag("Enemy"))
-            {
-                print("Hit enemy: " + other.name);
-            }
-            {
-                other.SendMessage("takeDamage", hitDamage);
-            }
-            Collider[] targets = Physics.OverlapSphere(this.transform.position, explosionRadius);
-            foreach (Collider target in targets)
-            {
-                print("Target: " + target.name + " Tag: " + target.tag);
-                Vector3 direction = (target.transform.position - this.transform.position).normalized;
-                if (target.CompareTag("Enemy"))
-                {
-                    target.SendMessage("takeDamage", explosionDamage);
-                    //target.SendMessage("enemyPushback", direction * explosionPushback);  Enemies need gravity + velocity to be pushed back  
-                }
-                if (target.name == "PlayerModel")
-                {
-                    Vector3 explosionOutput = new Vector3(
-                                            direction.x * explosionPushbackXZ,
-                                            direction.y * explosionPushbackY, // Reduced vertical pushback for player
-                                            direction.z * explosionPushbackXZ
-                                            );
-                    print("Player hit by explosion");
-                    player.SendMessage("getHit", playerDamage);
-                    player.SendMessage("applyPushback", explosionOutput);
-                }
-            }
-            trusterPs.Stop();
-            isExploded = true;
-            explosionPs.Play();
-            gameObject.GetComponent<Renderer>().enabled = false;
-            Destroy(gameObject, explosionPs.main.startLifetime.constant);
-        }
-    }
+        // if the collision is with anything it should collide with -> return
+        if (other.CompareTag("PlayerGroup") || other.CompareTag("PlasmaOrb") || other.CompareTag("Missile")) return;
+        
+        // direct hit on enemy
+        if (other.CompareTag("Enemy")) other.SendMessage("takeDamage", hitDamage);
 
+        //explosion logic
+        //due to structure the to identify the player we need to check for the "Ignore Raycast" layer, which aint optimal
+        Collider[] entitiesInRange = Physics.OverlapSphere(
+            transform.position,
+            explosionRadius,
+            LayerMask.GetMask("Enemies", "Ignore Raycast"),
+            QueryTriggerInteraction.Collide
+        );
+
+        foreach (Collider hit in entitiesInRange)
+        {
+            //player specific logic
+            if (hit.name == "PlayerModel")
+            {
+                Vector3 direction = (hit.transform.position - transform.position).normalized;
+                Vector3 outputPushVector = new Vector3(
+                    direction.x * explosionPushbackXZ,
+                    direction.y * explosionPushbackY,
+                    direction.z * explosionPushbackXZ
+                );
+                //doing it this way to avoid doing a FindObjectWithTag search, since our scenes are now quite chunky
+                hit.gameObject.GetComponentInParent<Player>().SendMessage("getHit", playerDamage);
+                hit.gameObject.GetComponentInParent<Player>().SendMessage("applyPushback", outputPushVector);
+            }
+            else
+            {
+                hit.gameObject.SendMessage("takeDamage", explosionDamage);
+            }
+        }
+
+        //stop moving particles
+        trusterPs.Stop();
+        //setup explosion and destruction
+        isExploded = true;                  //stops it from continuing moving while explosion happens
+        explosionPs.Play();
+        gameObject.GetComponent<Renderer>().enabled = false;
+        Destroy(gameObject, explosionPs.main.startLifetime.constant);
+    }
+    
 }
