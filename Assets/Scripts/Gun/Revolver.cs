2bebb2e00dabe7422b6aded215e2473434b25c56
Cyberburd <davidpirescoelho@gmail.com>
2025-06-14 22:28:51 +0100

    Fixed Revolver SFX

--- Assets/Scripts/Gun/Revolver.cs
+++ Assets/Scripts/Gun/Revolver.cs
@@ -2,14 +2,9 @@ using UnityEngine;
 
 public class Revolver : Gun
 {
+
     public override void shoot()
     {
-        // Plays the assigned SFX
-        if (fireSFX != null && audioSource != null)
-        {
-            audioSource.PlayOneShot(fireSFX);
-        }
-
         base.shoot();
     }
 
